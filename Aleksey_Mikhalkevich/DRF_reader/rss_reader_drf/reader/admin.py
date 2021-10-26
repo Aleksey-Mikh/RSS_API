46b3a670b88f14190fe4c25902310c92961ff677
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-26 22:33:27 +0300

    added Meta in model and register Model in admin

--- Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/admin.py
+++ Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News, Feed, SourceForParse
+
+
+admin.site.register(News)
+admin.site.register(Feed)
+admin.site.register(SourceForParse)
--- Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/models.py
+++ Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/models.py
@@ -6,23 +6,44 @@ class News(models.Model):
         "Feed", verbose_name="Channel title", on_delete=models.PROTECT, related_name="news"
     )
     title = models.CharField(max_length=200, verbose_name="Title")
-    date = models.TimeField(verbose_name="Publication date")
+    pub_date = models.TimeField(verbose_name="Publication date")
     link = models.URLField(max_length=200, verbose_name="Link")
-    author = models.CharField(max_length=200, verbose_name="Author")
-    category = models.CharField(max_length=255, verbose_name="Category")
-    description = models.CharField(max_length=255, verbose_name="Description")
+    author = models.CharField(max_length=200, verbose_name="Author", blank=True)
+    category = models.CharField(max_length=255, verbose_name="Category", blank=True)
+    description = models.CharField(max_length=255, verbose_name="Description", blank=True)
     more_description = models.TextField(verbose_name="More description", blank=True)
     comments = models.CharField(max_length=255, verbose_name="Comments", blank=True)
     media_objects = models.TextField(verbose_name="Media Object", blank=True)
     extra_links = models.TextField(verbose_name="Extra_links", blank=True)
     source_feed = models.TextField(verbose_name="Source", blank=True)
 
+    def __str__(self):
+        return f"{self.title}, {self.pub_date}"
+
+    class Meta:
+        verbose_name = "News"
+        verbose_name_plural = "News"
+
 
 class Feed(models.Model):
     channel_title = models.CharField(max_length=255, verbose_name="Channel title")
     source = models.URLField(max_length=200, verbose_name="Feed source")
 
+    def __str__(self):
+        return f"{self.channel_title}, {self.source}"
+
+    class Meta:
+        verbose_name = "Feed"
+        verbose_name_plural = "Feeds"
+
 
 class SourceForParse(models.Model):
     source_for_parse = models.URLField(max_length=200, verbose_name="Source for parse")
+
+    def __str__(self):
+        return self.source_for_parse
+
+    class Meta:
+        verbose_name = "Source for parse"
+        verbose_name_plural = "Sources for parse"
 

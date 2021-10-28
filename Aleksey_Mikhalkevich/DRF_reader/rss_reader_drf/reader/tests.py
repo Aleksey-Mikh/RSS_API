435796b99c8b7956c8d354d1cab2fc273e963246
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-28 15:27:29 +0300

    added tests

--- Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/tests.py
+++ Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+
+
+class TestApi(APITestCase):
+
+    def test_get_news_get_method(self):
+        url = reverse('news:get_news')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_news_post_method(self):
+        url = reverse('news:get_news')
+        data = {
+            "source": "https://people.onliner.by/feed",
+            "pub_date": "",
+            "limit": 1,
+            "json": False,
+            "to-pdf": False,
+            "to-html": False
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            response.data, {
+                "message": "You see this message because you enter false in JSON value."
+            }
+        )
+
+    def test_feeds_get_method(self):
+        url = reverse('news:feeds')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_feeds_pk_get_method(self):
+        url = reverse('news:feeds_pk', kwargs={"pk": 2})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_news_get_method(self):
+        url = reverse('news:news')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_news_pk_get_method(self):
+        url = reverse('news:news_pk', kwargs={"pk": 2})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/urls.py
+++ Aleksey_Mikhalkevich/DRF_reader/rss_reader_drf/reader/urls.py
@@ -16,9 +16,9 @@ app_name = "news"
 urlpatterns = [
     path('download_pdf/', DownloadPdfView.as_view(), name="download_pdf"),
     path('download_html/', DownloadHTMLView.as_view(), name="download_html"),
-    path('get_news/', GetNewsView.as_view()),
-    path('feeds/', FeedListView.as_view()),
-    path('feeds/<int:pk>', FeedDetailView.as_view()),
-    path('news/', NewsListView.as_view()),
-    path('news/<int:pk>', NewsDetailView.as_view()),
+    path('get_news/', GetNewsView.as_view(), name="get_news"),
+    path('feeds/', FeedListView.as_view(), name="feeds"),
+    path('feeds/<int:pk>', FeedDetailView.as_view(), name="feeds_pk"),
+    path('news/', NewsListView.as_view(), name="news"),
+    path('news/<int:pk>', NewsDetailView.as_view(), name="news_pk"),
 ]
